efe7a49dc7edb2c496c17bd81b61b29c274a7e2b
wein0034 <wein0034@flinders.edu.au>
2022-03-02 19:39:27 +1030

    Initial commit

---
+++ src/wein0034/Practical1/Question2.java
@@ -0,0 +1,13 @@
+package wein0034.Practical1;
+
+import java.util.Scanner;
+
+public class Question2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Work");
+		System.out.println("is");
+		System.out.println("Workship");
+	}
+}
